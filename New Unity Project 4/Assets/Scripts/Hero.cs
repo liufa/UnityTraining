db059fbd0c911011e0d3e22cf321349d92fd913b
liufa <mataswork@gmail.com>
2017-06-06 13:09:53 +0700

    Added panel and machanism to add buttons that does not how yet

--- New Unity Project 4/Assets/Scripts/Hero.cs
+++ New Unity Project 4/Assets/Scripts/Hero.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Hero : MonoBehaviour {
 
@@ -9,11 +10,28 @@ public class Hero : MonoBehaviour {
     // Use this for initialization
     public void Down()
     {
+
+        MakeButton("Down");
+
         var move = GameObject.Find("Hero").transform.position;
         animator.SetTrigger("moveDown");
 
-        transform.position = Vector3.Lerp(move, move + new Vector3(0, -1, 0), 1); 
+        transform.position = Vector3.Lerp(move, move + new Vector3(0, -1f, 0), 1f); 
 
         
+    }
+
+    public void MakeButton(string direction)
+    {
+        
+        GameObject button = new GameObject();
+        button.AddComponent<RectTransform>();
+        button.AddComponent<Button>();
+        button.transform.position = new Vector3(-0,0,0);
+        button.GetComponent<RectTransform>().SetParent(GameObject.Find("CommandPanel").transform);
+        button.SetActive(true);
+        
+        // button.active = false;
+        // (button.GetComponentInChildren(typeof(Text), true) as Text).text = direction;
     }
 }
